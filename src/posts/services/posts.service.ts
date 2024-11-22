029c4d9e30f60a31e50c99be6a985b00a84bd6b1
ozankenangungor <s220204044@ankarabilim.edu.tr>
2024-11-22 23:02:08 +0300

    Exception handling

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,4 +1,4 @@
-import { Body, Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Body, Inject, Injectable, RequestTimeoutException } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { Post } from '../post.entity';
 import { TagsService } from 'src/tags/providers/tags.service';
 import { PatchPostDto } from '../dtos/patch-post.dto';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 
 @Injectable()
 export class PostsService {
@@ -59,27 +60,74 @@ export class PostsService {
     }
 
     public async update(patchPostDto: PatchPostDto) {
-        // Find new tags
-        let tags = await this.tagsService.findMultipleTags(patchPostDto.tags);
+        let tags = undefined;
+        let post = undefined;
     
-        // Find the post
-        let post = await this.postRepository.findOneBy({
+        // Find the Tags
+        try {
+          tags = await this.tagsService.findMultipleTags(patchPostDto.tags);
+        } catch (error) {
+          throw new RequestTimeoutException(
+            'Unable to process your request at the moment please try later',
+            {
+              description: 'Error connecting to the database',
+            },
+          );
+        }
+    
+        /**
+         * If tags were not found
+         * Need to be equal number of tags
+         */
+        if (!tags || tags.length !== patchPostDto.tags.length) {
+          throw new BadRequestException(
+            'Please check your tag Ids and ensure they are correct',
+          );
+        }
+         // Find the Post
+    try {
+        // Returns null if the post does not exist
+        post = await this.postRepository.findOneBy({
           id: patchPostDto.id,
         });
+      } catch (error) {
+        throw new RequestTimeoutException(
+          'Unable to process your request at the moment please try later',
+          {
+            description: 'Error connecting to the database',
+          },
+        );
+      }
+  
+      if (!post) {
+        throw new BadRequestException('The post Id does not exist');
+      }
+  
+      // Update the properties
+      post.title = patchPostDto.title ?? post.title;
+      post.content = patchPostDto.content ?? post.content;
+      post.status = patchPostDto.status ?? post.status;
+      post.postType = patchPostDto.postType ?? post.postType;
+      post.slug = patchPostDto.slug ?? post.slug;
+      post.featuredImageUrl =
+        patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
+      post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+  
+      // Assign the new tags
+      post.tags = tags;
+  
+      // Save the post and return
+      try {
+        await this.postRepository.save(post);
+      } catch (error) {
+        throw new RequestTimeoutException(
+          'Unable to process your request at the moment please try later',
+          {
+            description: 'Error connecting to the database',
+          },
+        );
+      }
+      return post;
+    }
     
-        // Update post related properties
-        post.title = patchPostDto.title ?? post.title;
-        post.content = patchPostDto.content ?? post.content;
-        post.status = patchPostDto.status ?? post.status;
-        post.postType = patchPostDto.postType ?? post.postType;
-        post.slug = patchPostDto.slug ?? post.slug;
-        post.featuredImageUrl =
-          patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
-        post.publishOn = patchPostDto.publishOn ?? post.publishOn;
-    
-        // Update the tags
-        post.tags = tags;
-    
-        return await this.postRepository.save(post);
-      }x
 }
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,12 +1,13 @@
 import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUsersParamDto } from './../dtos/get-user-params.dto';
-import { forwardRef, Inject, Injectable } from "@nestjs/common";
-import { Repository } from 'typeorm';
+import { BadRequestException, forwardRef, HttpException, HttpStatus, Inject, Injectable, RequestTimeoutException } from "@nestjs/common";
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { ConfigService, ConfigType } from '@nestjs/config';
 import profileConfig from '../config/profile.config';
+import { error } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -16,36 +17,95 @@ export class UsersService {
         @InjectRepository(User)
         private usersRepository: Repository<User>,
         @Inject(profileConfig.KEY)
-        private readonly profileConfiguration: ConfigType<typeof profileConfig>
+        private readonly profileConfiguration: ConfigType<typeof profileConfig>,
+
+        private readonly dataSource: DataSource
     ){}
 
     public async createUser(createUserDto: CreateUserDto) {
-        //Check if user exists with same email
-        const existingUser = await this.usersRepository.findOne({
+        let existingUser = undefined;
+    
+        try {
+          // Check if user with email exists
+          existingUser = await this.usersRepository.findOne({
             where: {
                 email: createUserDto.email
-            }
-        });
-
-        //Create a new user
+            },
+          });
+        } catch (error) {
+          // Might want to save these errors with more information in a log file or database
+          // You don't need to send this sensitive information to user
+          throw new RequestTimeoutException(
+            'Unable to process your request at the moment please try later',
+            {
+              description: 'Error connecting to database',
+            },
+          );
+        }
+    
+        /**
+         * Handle exceptions if user exists later
+         * */
+        if (existingUser) {
+          throw new BadRequestException(
+            'The user already exists, please check your email',
+          );
+        }
+    
+        // Try to create a new user
+        //  Handle Exceptions Later
         let newUser = this.usersRepository.create(createUserDto);
-        return await this.usersRepository.save(newUser);
-        
+
+    try {
+      newUser = await this.usersRepository.save(newUser);
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to database',
+        },
+      );
     }
+
+    // Create the user
+    return newUser;
+  }
+
 
     public async findOneById(id: number) {
         return await this.usersRepository.findOneBy({id})
     }
     
-    
     public findAll(
         getUserParamDto: GetUsersParamDto,
-        limit: number,
+        limt: number,
         page: number,
-    ){
-       // test the new config
-       console.log(this.profileConfiguration)
+      ) {
+        // test the new config
+       //console.log(this.profileConfiguration)
+
+       // Custom error
+        let loggenIn = false;
+        if (!loggenIn) {
+          throw new HttpException(
+            {
+              status: HttpStatus.MOVED_PERMANENTLY,
+              error: `The API endpoint doesn't exist anymore`,
+              fileName: 'users.service.ts',
+              lineNumber: 103,
+            },
+            HttpStatus.MOVED_PERMANENTLY,
+            {
+              cause: new Error(),
+              description:
+                'Occured because the API endpoint was permanently moved to a new location',
+            },
+          );
+        }
+      }
+
+    
     }
  
     
-}
+
